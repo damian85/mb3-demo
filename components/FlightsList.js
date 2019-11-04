aab545edcc7af6f52c984c8edfabcb1bddcf8206
Nahuel Castro <nahukas@gmail.com>
2019-11-04 20:36:09 -0300

    chupala

--- components/FlightsList.js
+++ components/FlightsList.js
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import { View, Text, FlatList } from 'react-native';
 
 const FlightsList = props => {
-
-    return (
-     /*<FlatList 
-        data={props.flights}
-        keyExtractor={item => item.id}
-        renderItem={itemData => <Text>{itemData.item.id} - {itemData.item.from} to {itemData.item.to} </Text>}
-     />  */
-     <Text>{props.flights}</Text> 
-    );
+  return (
+    <FlatList
+      data={props.flights}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <Text>
+          {itemData.item.id} - {itemData.item.from} to {itemData.item.to}{' '}
+        </Text>
+      )}
+    />
+    /*<Text>{props.flights}</Text> */
+  );
 };
 
 export default FlightsList;
